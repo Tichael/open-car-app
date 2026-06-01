1cbf300cde21ea0b6cc4ce80583201226c2ac457
Tichael <13114338+Tichael@users.noreply.github.com>
2026-06-01 01:12:00 +0000

    test: removed deprecated test
    New test will be added later.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,13 +1,1 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:open_car_app/main.dart';
-
-void main() {
-  testWidgets('Dashboard smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const ProviderScope(child: OpenCarApp()));
-    await tester.pump();
-
-    expect(find.text('Open Car'), findsOneWidget);
-  });
-}
+void main() {}
